c9e465f88e0c7ff345615a26692cb603c80f92a6
iglooe <day.derek7@protonmail.com>
2023-11-29 22:34:19 -0600

    .

--- Windows/C slash Windows/_DebloatScript.ps1
+++ Windows/C slash Windows/_DebloatScript.ps1
@@ -4,14 +4,14 @@ Get-AppxPackage -allusers Microsoft.BingNews | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.BingWeather | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.GetHelp | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.Getstarted | Remove-AppxPackage
-Get-AppxPackage -allusers Microsoft.Microsoft3DViewer | Remove-AppxPackage
+# Get-AppxPackage -allusers Microsoft.Microsoft3DViewer | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.MixedReality.Portal | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.MSPaint | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.Office.OneNote | Remove-AppxPackage
-Get-AppxPackage -allusers Microsoft.OneDriveSync | Remove-AppxPackage
+# Get-AppxPackage -allusers Microsoft.OneDriveSync | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.People | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.PowerAutomateDesktop | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.ScreenSketch | Remove-AppxPackage
@@ -19,7 +19,7 @@ Get-AppxPackage -allusers Microsoft.SkypeApp | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.Todos | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.Wallet | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.WindowsAlarms | Remove-AppxPackage
-Get-AppxPackage -allusers Microsoft.WindowsCalculator | Remove-AppxPackage
+# Get-AppxPackage -allusers Microsoft.WindowsCalculator | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.WindowsCamera | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.WindowsFeedbackHub | Remove-AppxPackage
 Get-AppxPackage -allusers Microsoft.WindowsMaps | Remove-AppxPackage
@@ -31,7 +31,7 @@ Get-AppxPackage -allusers MicrosoftTeams | Remove-AppxPackage
 Get-AppxPackage -allusers MicrosoftWindows.Client.WebExperience | Remove-AppxPackage
 Get-AppxPackage -allusers SpotifyAB.SpotifyMusic | Remove-AppxPackage
 Get-AppxPackage -allusers microsoft.windowscommunicationsapps | Remove-AppxPackage
-C:\Windows\SysWOW64\OneDriveSetup.exe -uninstall
+# C:\Windows\SysWOW64\OneDriveSetup.exe -uninstall
 Get-WindowsPackage -Online | Where PackageName -like *Hello-Face* | Remove-WindowsPackage -Online -NoRestart
 Get-WindowsPackage -Online | Where PackageName -like *QuickAssist* | Remove-WindowsPackage -Online -NoRestart
 pause
